85d5508a7275bc855e2bf9204bc4c4941268722d
Flatus <167695656+Flatuitous@users.noreply.github.com>
2024-05-02 23:40:41 +1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ let objToRender = 'car';
 const loader = new GLTFLoader();
 
 loader.load(
-  `models/${objToRender}/scene.gltf`,
+  `models/${objToRender}.gltf`,
   function (gltf) {
     // if file loaded, add to scene
     object = gltf.scene;
